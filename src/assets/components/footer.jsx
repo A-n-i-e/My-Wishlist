6a6aa94792a04a9d7856129c0c976c1818a66a38
A-n-i-e <anie.emenike@gmail.com>
2025-09-07 00:01:34 +0100

    footer edit

--- src/assets/components/footer.jsx
+++ src/assets/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full text-center bg-transparent text-sky-blue p-4  mt-4 font-lazydog border-t-2 border-sky-blue ">
-      <p>Made with tears and love 🥹❤️ by Stephanie. <br/>Arigato Gosimasu!❤️</p>
+      <p>Made with tears and love 🥹❤️ by Stephanie. <br/>Arigato Gozaimasu!❤️</p>
     </footer>
   );
 }
